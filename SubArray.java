b6f5790f419ce809b97f870e668bb27a6d5aec0a
Charu <vardhancharu8@gmail.com>
2019-11-14 11:48:57 +0530

    code

--- SubArray.java
+++ SubArray.java
@@ -1,26 +1,50 @@
-import java.util.*
-public class SubArray
+import java.util.;
+public class subArray
 {
-    public static void main(String[] args)
+
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int arr[]=new int[5];
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int sum[]=new int[nn];
         int k=sc.nextInt();
-        for(int i=0;i<5;i++)
+
+        for(int i=0;in;i++)
         {
             arr[i]=sc.nextInt();
         }
-        for(int j=1;j<5;j++)
-        {
-            for(int i=0;i<j;i++)
-            {
-                if(arr[i]>=4)
-                System.out.println(arr[i]);
-
-            }
-        }
 
 
+
+        int s;
+        n=n-k+1;
+        s=(n(n+2))2;
+        System.out.print(The Number of subarrays with length +k+ is = );
+        System.out.println(s);
+        int j=-1;
+         int sum=0;
+         int c=0;
+         for(int i=0;in;i++)
+         {
+             for(int j=0;jn;j++)
+             {
+                 sum=sum+arr[j];
+                 System.out.print(sum+ );
+              if(sum=k)
+              {
+                  c++;
+              }
+             }
+             sum=0;
+         }
+
+      System.out.println(    +c);
+        
 
     }
+
+
+
+
 }
